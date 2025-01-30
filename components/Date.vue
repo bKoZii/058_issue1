415b9a5b035c3d78a01ab5b2142f82afd5f915fd
bKoZii <bigkonkamon@gmail.com>
2025-01-30 09:31:23 +0700

    feat: :sparkles: เพิ่มไฟล์ Date / LoginForm

---
+++ components/Date.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <h2>
+      <center>
+        <font color="#FFFFFF">วันที่ 1 มกราคม 2564</font>
+      </center>
+    </h2>
+  </div>
+</template>
---
+++ components/LoginForm.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <input type="text" name="username" id="username" placeholder="Username">
+    <input type="password" name="password" id="password" placeholder="Password">
+  </div>
+</template>
